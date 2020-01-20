ff014f7e72abb1c6a8bdc054d788be99630b2ba2
Anders <Luzr@users.noreply.github.com>
2020-01-20 14:00:11 -0600

    Updated path for var and move the print newline.

--- qid-pid.py
+++ qid-pid.py
@@ -5,15 +5,15 @@
 import os
 import csv
 
-_path = '.'
+_path = '.\\photos\\'
 with open('qid_and_pid.csv') as f:
     lines = csv.reader(f)
     for line in lines:
         #print(line[0], line[1])
         for filename in os.listdir(_path):
             if(line[0] in filename):
+                print('\n')
                 print("Match: {}, {}, {}".format(line[0] ,filename, line[1]))
                 rename = filename
-                os.rename(rename, line[1]+'.jpg')
-                print('\n')
+                os.rename(_path+rename, _path+line[1]+'.jpg')
                 print("File {} renamed to {}".format(rename,line[1] ))
